7d0f8269073d62fe67e3e4d2ab214804dfd69155
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-12-28 21:32:30 +0530

    Update inline.py

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 from config import SUPPORT_GRP, UPDATE_CHNL
-from Mickey import OWNER
+from Mickey import COOWNER
 from Mickey import MickeyBot
 
 DEV_OP = [
