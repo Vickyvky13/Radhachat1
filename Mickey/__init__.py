0974de8db7e3a84db0b4af69e9b1c2a69184e620
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2023-12-29 08:16:51 +0530

    Update __init__.py

--- Mickey/__init__.py
+++ Mickey/__init__.py
@@ -5,7 +5,7 @@ from Abg import patch
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import Client
 from pyrogram.enums import ParseMode
-
+from pyrogram import app
 import config
 
 logging.basicConfig(
